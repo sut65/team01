d9416e3f6baa408eb53a065b1048f5ae86865f08
Siriwan38 <Ae44120@icloud.com>
2023-01-25 23:30:04 +0700

    entity Payment - close #43

---
+++ backend/entity/Payment.go
@@ -0,0 +1,38 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Payment struct {
+	gorm.Model
+
+	PatientRightID *uint
+
+	PatientOfRight PatientOfRight `gorm:"references:id" valid:"-"`
+
+	PaymentTypeID *uint
+
+	PaymentType PaymentType `gorm:"references:id" valid:"-"`
+
+	PaymentTime time.Time
+
+	Total uint `valid:"required~Total cannot be zero"`
+
+	EmployeeID *uint
+	Employee   Employee `gorm:"references:id" valid:"-"`
+
+	//MedicineRecordID ทำหน้าที่เป็น ForeignKey
+	MedicineRecordID *uint
+	MedicineRecord   MedicineRecord `gorm:"references:id" valid:"-"`
+}
+
+type PaymentType struct {
+	gorm.Model
+
+	Type string
+
+	Payments []Payment `gorm:"foreignKey:PayTypeID"`
+}
