bd6e08bf05358cfed4dd8d1d5702287ea5ab3a79
PimchanokS <pimchanok447@gmail.com>
2023-02-03 21:32:53 +0700

    edit Controller-Gender - close #107

--- backend/controller/gender.go
+++ backend/controller/gender.go
@@ -6,11 +6,23 @@ import (
 	"net/http"
 )
 
-func ListGender(c *gin.Context){
-	var Gender []entity.Gender
-	if err := entity.DB().Table("genders").Find(&Gender).Error; err != nil{
+func GetGender(c *gin.Context) {
+	var gender entity.Gender
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM genders WHERE id = ?", id).Scan(&gender).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK,gin.H{"data": Gender})
+	c.JSON(http.StatusOK, gin.H{"data":gender})
 }
+
+func ListGender(c *gin.Context) {
+	var gender []entity.Gender
+	if err := entity.DB().Raw("SELECT * FROM genders").Scan(&gender).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": gender})
+}
+
