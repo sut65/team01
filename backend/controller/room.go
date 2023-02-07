ea1c552847ea7dcf1b9c1c752c326dfe304fff9c
PimchanokS <pimchanok447@gmail.com>
2023-02-07 16:05:39 +0700

    Controller-Room - close #113

---
+++ backend/controller/room.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"net/http"
+	"github.com/sut65/team01/entity"
+	"github.com/gin-gonic/gin"
+)
+
+func GetRoom(c *gin.Context) {
+	var room entity.Room
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM rooms WHERE id = ?", id).Scan(&room).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data":room})
+}
+
+func ListRoom(c *gin.Context) {
+	var room []entity.Room
+	if err := entity.DB().Raw("SELECT * FROM rooms").Scan(&room).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": room})
+}
+
