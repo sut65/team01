e286016d6f2a76e78e8952073304f9da32cdae7f
A KiRA <akiraphat_j63@hotmail.com>
2023-02-14 22:55:05 +0700

    ทำ validation ของ field Date ใน TreatmentRecord - close #70

--- backend/entity/treatmentrecord_validation_test.go
+++ backend/entity/treatmentrecord_validation_test.go
@@ -9,13 +9,11 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-
-
 func TestTreatmentNotBlank(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	treatment := TreatmentRecord{
-		Note:              "",
+		Note:             "",
 		Treatment:        "", //wrong
 		MedicineQuantity: 20,
 		Date:             time.Now(),
@@ -38,7 +36,7 @@ func TestMedQuantNotNegative(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	treatment := TreatmentRecord{
-		Note:              "",
+		Note:             "",
 		Treatment:        "yes",
 		MedicineQuantity: -56,
 		Date:             time.Now(),
@@ -58,26 +56,25 @@ func TestMedQuantNotNegative(t *testing.T) {
 
 }
 
-// func TestDateMustbeNotPast(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestDateMustNotbePast(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	n, _ := time.Parse("02 Jan 2006", "03 Feb 2022")
-// 	treatment := TreatmentRecord{
-// 		Note:              "",
-// 		Treatment:        "yes",
-// 		MedicineQuantity: 9,
-// 		Date:             n,
-// 	}
+	treatment := TreatmentRecord{
+		Note:             "",
+		Treatment:        "yes",
+		MedicineQuantity: 9,
+		Date:             time.Now().Add(-34*time.Minute),
+	}
 
-// 	// ตรวจสอบด้วย govalidation
-// 	ok, err := govalidator.ValidateStruct(treatment)
+	// ตรวจสอบด้วย govalidation
+	ok, err := govalidator.ValidateStruct(treatment)
 
-// 	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
-// 	g.Expect(ok).ToNot(BeTrue())
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
 
-// 	// err ต้องไม่เป็นค่า nil แปลว่าจ้องจับ error ได้
-// 	g.Expect(err).ToNot(BeNil())
+	// err ต้องไม่เป็นค่า nil แปลว่าจ้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
 
-// 	// err.Error ต้องมี error message แสดงออกมา
-// 	g.Expect(err.Error()).To(Equal("Recording time must be current"))
-// }
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("Date must not be past"))
+}
