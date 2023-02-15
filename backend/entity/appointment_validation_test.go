00847a5d56065ee1e43f173a2a8be71462e8bf94
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-02-15 19:47:17 +0700

    edit entity and controller sprint 2 - close #157

--- backend/entity/appointment_validation_test.go
+++ backend/entity/appointment_validation_test.go
@@ -1,127 +1,127 @@
 package entity
 
-// import (
-// 	"testing"
-// 	"time"
+import (
+	"testing"
+	"time"
 
-// 	"github.com/asaskevich/govalidator"
-// 	. "github.com/onsi/gomega"
-// )
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
 
-// func TestAppointmentPass(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestAppointmentPass(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	//ข้อมูลที่ถูกต้องหมดทุก field
-// 	appointment := Appointment{
-// 		AppointmentTime: time.Date(2023, 1, 1, 12, 00, 00, 00, time.UTC),
-// 		Note:            "ฉีดวัคซีน",
-// 		RoomNumber:      111,
-// 	}
+	//ข้อมูลที่ถูกต้องหมดทุก field
+	appointment := Appointment{
+		AppointmentTime: time.Date(2023, 1, 1, 12, 00, 00, 00, time.UTC),
+		Note:            "ฉีดวัคซีน",
+		RoomNumber:      111,
+	}
 
-// 	//ตรวจสอบ govalidator
-// 	ok, err := govalidator.ValidateStruct(appointment)
+	//ตรวจสอบ govalidator
+	ok, err := govalidator.ValidateStruct(appointment)
 
-// 	// ok ต้องเป็น true แปลว่าไม่มี error
-// 	g.Expect(ok).To(BeTrue())
+	// ok ต้องเป็น true แปลว่าไม่มี error
+	g.Expect(ok).To(BeTrue())
 
-// 	// err เป็นค่า nil แปลว่าไม่มี error
-// 	g.Expect(err).To(BeNil())
-// }
+	// err เป็นค่า nil แปลว่าไม่มี error
+	g.Expect(err).To(BeNil())
+}
 
-// func TestAppointmentTime(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestAppointmentTime(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	apmt := []time.Time{
-// 		time.Now(), //ผิดเพราะเวลาต้องไม่เป็นอดีตและปัจจุบัน
-// 		time.Date(2019, 1, 1, 12, 00, 00, 00, time.UTC), //ผิดเพราะเวลาต้องไม่เป็นอดีตและปัจจุบัน
-// 	}
+	apmt := []time.Time{
+		time.Now(), //ผิดเพราะเวลาต้องไม่เป็นอดีตและปัจจุบัน
+		time.Date(2019, 1, 1, 12, 00, 00, 00, time.UTC), //ผิดเพราะเวลาต้องไม่เป็นอดีตและปัจจุบัน
+	}
 
-// 	for _, apmt := range apmt {
-// 		appointment := Appointment{
-// 			AppointmentTime: apmt,
-// 			Note:            "ฉีดวัคซีน",
-// 			RoomNumber:      111,
-// 		}
+	for _, apmt := range apmt {
+		appointment := Appointment{
+			AppointmentTime: apmt,
+			Note:            "ฉีดวัคซีน",
+			RoomNumber:      111,
+		}
 
-// 		//ตรวจสอบ govalidator
-// 		ok, err := govalidator.ValidateStruct(appointment)
+		//ตรวจสอบ govalidator
+		ok, err := govalidator.ValidateStruct(appointment)
 
-// 		// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
-// 		g.Expect(ok).ToNot(BeTrue())
+		// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
+		g.Expect(ok).ToNot(BeTrue())
 
-// 		// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
-// 		g.Expect(err).ToNot(BeNil())
+		// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
+		g.Expect(err).ToNot(BeNil())
 
-// 		// err.Error ต้องมี error message แสดงออกมา
-// 		g.Expect(err.Error()).To(Equal("AppointmentTime must be in the future"))
+		// err.Error ต้องมี error message แสดงออกมา
+		g.Expect(err.Error()).To(Equal("AppointmentTime must be in the future"))
 
-// 	}
-// }
+	}
+}
 
-// func TestAppointmentNote(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestAppointmentNote(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	appointment := Appointment{
-// 		AppointmentTime: time.Date(2023, 1, 1, 12, 00, 00, 00, time.UTC),
-// 		Note:            "", //ผิดต้องไม่เป็นข้อมูลว่าง
-// 		RoomNumber:      111,
-// 	}
+	appointment := Appointment{
+		AppointmentTime: time.Date(2023, 1, 1, 12, 00, 00, 00, time.UTC),
+		Note:            "", //ผิดต้องไม่เป็นข้อมูลว่าง
+		RoomNumber:      111,
+	}
 
-// 	//ตรวจสอบ govalidator
-// 	ok, err := govalidator.ValidateStruct(appointment)
+	//ตรวจสอบ govalidator
+	ok, err := govalidator.ValidateStruct(appointment)
 
-// 	// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
-// 	g.Expect(ok).ToNot(BeTrue())
+	// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
 
-// 	// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
-// 	g.Expect(err).ToNot(BeNil())
+	// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
 
-// 	// err.Error ต้องมี error message แสดงออกมา
-// 	g.Expect(err.Error()).To(Equal("Note cannot be blank"))
-// }
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("Note cannot be blank"))
+}
 
-// func TestAppointmentRoomNumber(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestAppointmentRoomNumber(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	appointment := Appointment{
-// 		AppointmentTime: time.Date(2023, 1, 1, 12, 00, 00, 00, time.UTC),
-// 		Note:            "ฉีดวัคซีน",
-// 		RoomNumber:      -1, //ผิดต้องมีค่ามากกว่า 0
-// 	}
+	appointment := Appointment{
+		AppointmentTime: time.Date(2023, 1, 1, 12, 00, 00, 00, time.UTC),
+		Note:            "ฉีดวัคซีน",
+		RoomNumber:      -1, //ผิดต้องมีค่ามากกว่า 0
+	}
 
-// 	//ตรวจสอบ govalidator
-// 	ok, err := govalidator.ValidateStruct(appointment)
+	//ตรวจสอบ govalidator
+	ok, err := govalidator.ValidateStruct(appointment)
 
-// 	// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
-// 	g.Expect(ok).ToNot(BeTrue())
+	// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
 
-// 	// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
-// 	g.Expect(err).ToNot(BeNil())
+	// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
 
-// 	// err.Error ต้องมี error message แสดงออกมา
-// 	g.Expect(err.Error()).To(Equal("RoomNumber more than 0"))
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("RoomNumber more than 0"))
 
-// }
+}
 
-// func TestAppointmentRoomNumbernotblank(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestAppointmentRoomNumbernotblank(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	appointment := Appointment{
-// 		AppointmentTime: time.Date(2023, 1, 1, 12, 00, 00, 00, time.UTC),
-// 		Note:            "ฉีดวัคซีน",
-// 		RoomNumber:      0, //ผิดต้องไม่เป็นค่าว่างหรือ0
-// 	}
+	appointment := Appointment{
+		AppointmentTime: time.Date(2023, 1, 1, 12, 00, 00, 00, time.UTC),
+		Note:            "ฉีดวัคซีน",
+		RoomNumber:      0, //ผิดต้องไม่เป็นค่าว่างหรือ0
+	}
 
-// 	//ตรวจสอบ govalidator
-// 	ok, err := govalidator.ValidateStruct(appointment)
+	//ตรวจสอบ govalidator
+	ok, err := govalidator.ValidateStruct(appointment)
 
-// 	// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
-// 	g.Expect(ok).ToNot(BeTrue())
+	// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
 
-// 	// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
-// 	g.Expect(err).ToNot(BeNil())
+	// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
 
-// 	// err.Error ต้องมี error message แสดงออกมา
-// 	g.Expect(err.Error()).To(Equal("RoomNumber cannot be blank"))
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("RoomNumber cannot be blank"))
 
-// }
+}
--- backend/entity/workload_validation_test.go
+++ backend/entity/workload_validation_test.go
@@ -1,87 +1,87 @@
 package entity
 
-// import (
-// 	"testing"
-// 	"time"
+import (
+	"testing"
+	"time"
 
-// 	"github.com/asaskevich/govalidator"
-// 	. "github.com/onsi/gomega"
-// )
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
 
-// func TestWorkloadPass(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestWorkloadPass(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	// ข้อมูลถูกต้องหมดทุก field
-// 	workload := &Workload{
-// 		Date:      time.Now(),
-// 		StartTime: time.Now().Add(1 * time.Hour),
-// 		EndTime:   time.Now().Add(2 * time.Hour),
-// 	}
+	// ข้อมูลถูกต้องหมดทุก field
+	workload := &Workload{
+		Date:      time.Now(),
+		StartTime: time.Now().Add(1 * time.Hour),
+		EndTime:   time.Now().Add(2 * time.Hour),
+	}
 
-// 	// ตรวจสอบด้วย govalidator
-// 	ok, err := govalidator.ValidateStruct(workload)
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(workload)
 
-// 	// ok ต้องเป็น true แปลว่าไม่มี error
-// 	g.Expect(ok).To(BeTrue())
+	// ok ต้องเป็น true แปลว่าไม่มี error
+	g.Expect(ok).To(BeTrue())
 
-// 	// err เป็นค่า nil แปลว่าไม่มี error
-// 	g.Expect(err).To(BeNil())
-// }
+	// err เป็นค่า nil แปลว่าไม่มี error
+	g.Expect(err).To(BeNil())
+}
 
-// func TestDateNotbePastandFuture(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestDateNotbePastandFuture(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	fixtures := []time.Time{
-// 		time.Now().Add(24 * time.Hour),
-// 		time.Now().Add(-24 * time.Hour),
-// 	}
+	fixtures := []time.Time{
+		time.Now().Add(24 * time.Hour),
+		time.Now().Add(-24 * time.Hour),
+	}
 
-// 	for _, fixture := range fixtures {
-// 		workload := &Workload{
-// 			Date:      fixture,
-// 			StartTime: time.Now().Add(1 * time.Hour),
-// 			EndTime:   time.Now().Add(2 * time.Hour),
-// 		}
-// 		ok, err := govalidator.ValidateStruct(workload)
+	for _, fixture := range fixtures {
+		workload := &Workload{
+			Date:      fixture,
+			StartTime: time.Now().Add(1 * time.Hour),
+			EndTime:   time.Now().Add(2 * time.Hour),
+		}
+		ok, err := govalidator.ValidateStruct(workload)
 
-// 		// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
-// 		g.Expect(ok).ToNot(BeTrue())
+		// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+		g.Expect(ok).ToNot(BeTrue())
 
-// 		// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
-// 		g.Expect(err).ToNot(BeNil())
+		// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+		g.Expect(err).ToNot(BeNil())
 
-// 		// err.Error() ต้องมี message แสดงออกมา
-// 		g.Expect(err.Error()).To(Equal("Date must be present"))
-// 	}
-// }
+		// err.Error() ต้องมี message แสดงออกมา
+		g.Expect(err.Error()).To(Equal("Date must be present"))
+	}
+}
 
-// func TestStartTimeMustbeFuture(t *testing.T){
-// 	g := NewGomegaWithT(t)
+func TestStartTimeMustbeFuture(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	workload:=Workload{
-// 		Date: time.Now(),
-// 		StartTime: time.Now().Add(-time.Hour),
-// 		EndTime: time.Now().Add(time.Hour),
-// 	}
+	workload := Workload{
+		Date:      time.Now(),
+		StartTime: time.Now().Add(-time.Hour),
+		EndTime:   time.Now().Add(time.Hour),
+	}
 
-// 	ok, err:= govalidator.ValidateStruct(workload)
-// 	g.Expect(ok).ToNot(BeTrue())
-// 	g.Expect(err.Error).ToNot(BeNil())
-// 	g.Expect(err.Error()).To(Equal("Start Time must be future"))
-// }
+	ok, err := govalidator.ValidateStruct(workload)
+	g.Expect(ok).ToNot(BeTrue())
+	g.Expect(err.Error).ToNot(BeNil())
+	g.Expect(err.Error()).To(Equal("Start Time must be future"))
+}
 
-// func TestEndTimeMustbeFuture(t*testing.T){
+func TestEndTimeMustbeFuture(t *testing.T) {
 
-// 	g:=NewGomegaWithT(t)
+	g := NewGomegaWithT(t)
 
-// 	workload:=Workload{
-// 		Date: time.Now(),
-// 		StartTime: time.Now().Add(time.Hour),
-// 		EndTime: time.Now().Add(-time.Hour),
-// 	}
+	workload := Workload{
+		Date:      time.Now(),
+		StartTime: time.Now().Add(time.Hour),
+		EndTime:   time.Now().Add(-time.Hour),
+	}
 
-// 	ok, err:=govalidator.ValidateStruct(workload)
-// 	g.Expect(ok).NotTo(BeTrue())
-// 	g.Expect(err.Error()).NotTo(BeNil())
-// 	g.Expect(err.Error()).To(Equal("End Time must be future"))
-// }
+	ok, err := govalidator.ValidateStruct(workload)
+	g.Expect(ok).NotTo(BeTrue())
+	g.Expect(err.Error()).NotTo(BeNil())
+	g.Expect(err.Error()).To(Equal("End Time must be future"))
+}
