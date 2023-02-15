484aa3aca6c96f01fe7847ca3c5744a2725600e9
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-02-15 13:35:36 +0700

    entity PatientRegiater - close #56

--- backend/entity/Employee.go
+++ backend/entity/Employee.go
@@ -2,33 +2,35 @@ package entity
 
 import (
 	"time"
+
+	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
 
 type Admin struct {
 	gorm.Model
-	FirstName string 
+	FirstName string
 	LastName  string
 	Email     string
 	Password  string
-	Employee []Employee `gorm:"foreignKey:AdminID"`
+	Employee  []Employee `gorm:"foreignKey:AdminID"`
 }
 
 type Title struct {
 	gorm.Model
-	Name      string
+	Name     string
 	Employee []Employee `gorm:"foreignKey:TitleID"`
 }
 
 type Role struct {
 	gorm.Model
-	Name string
-	Employee    []Employee `gorm:"foreignKey:RoleID"`
+	Name     string
+	Employee []Employee `gorm:"foreignKey:RoleID"`
 }
 
 type Gender struct {
 	gorm.Model
-	Name      string
+	Name     string
 	Employee []Employee `gorm:"foreignKey:GenderID"`
 }
 
@@ -39,31 +41,34 @@ type Employee struct {
 	Admin   Admin
 	IDCard  string `gorm:"uniqueIndex" valid:"required~Identification Number cannot be blank, matches(^[1-9]\\d{12}$)~IDCard does not validate"`
 	//TitleID ทำหน้าที่ FK
-	TitleID *uint
-	Title   Title
+	TitleID   *uint
+	Title     Title
 	FirstName string `valid:"required~First Name cannot be blank"`
 	LastName  string `valid:"required~Last Name cannot be blank"`
 	//RoleID ทำหน้าที่ FK
-	RoleID  *uint
-	Role    Role
+	RoleID      *uint
+	Role        Role
 	PhoneNumber string `valid:"required~Phone Number cannot be blank, matches(^[0]{1}[689]{1}[0-9]{8})~Phone Number must be invalid"`
 	Email       string `gorm:"uniqueIndex" valid:"required~Email cannot be blank, email~Email does not validate as email"`
 	Password    string `valid:"required~Password cannot be blank"`
 	//GenderID ทำหน้าที่ FK
 	GenderID *uint
 	Gender   Gender
-	Salary 	float64	`valid:"required~Salary must not be zero,salary~Salary must not be negative"`
-	Birthday    time.Time `valid:"past~Birthday: The following validator is invalid or can't be applied to the field: \"past\""`
+	Salary   float64   `valid:"required~Salary must not be zero,salary~Salary must not be negative"`
+	Birthday time.Time `valid:"past~Birthday: The following validator is invalid or can't be applied to the field: \"past\""`
+
+	PatientRegisters []PatientRegister `gorm:"foreignKey:EmployeeID"`
 }
+
 func init() {
 	govalidator.CustomTypeTagMap.Set("past", func(i interface{}, context interface{}) bool {
 		t := i.(time.Time)
 		return t.Before(time.Now())
 	})
-	
+
 	govalidator.CustomTypeTagMap.Set("salary", func(i interface{}, context interface{}) bool {
 		t := i.(float64)
 		return t >= 1
 	})
-	
+
 }
