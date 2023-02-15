50dbd1f6c8fc4af03d4bb01c809929d84aa59a6f
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-02-15 14:44:39 +0700

    controller prefix of PatientRegister - close #146

---
+++ backend/controller/patientregister/prefix.go
@@ -0,0 +1,143 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut65/team01/entity"
+)
+
+/*	POST /users
+	func CreateUser(c *gin.Context) {
+		var user entity.User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if err := entity.DB().Create(&user).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	}
+*/
+
+// POST /prefixes
+func CreatePrefix(c *gin.Context) {
+	var prefix entity.Prefix
+	if err := c.ShouldBindJSON(&prefix); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := entity.DB().Create(&prefix).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": prefix})
+}
+
+// *******************************************************************************************************
+
+// GET /user/:id
+// func GetUser(c *gin.Context) {
+// 	var user entity.User
+// 	id := c.Param("id")
+// 	if err := entity.DB().Raw("SELECT * FROM users WHERE id = ?", id).Scan(&user).Error; err != nil {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		   return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": user})
+// }
+
+// GET /prefix/:id
+func GetPrefix(c *gin.Context) {
+	var prefix entity.Prefix
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM prefixes WHERE id = ?", id).Scan(&prefix).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": prefix})
+}
+
+// *******************************************************************************************************
+
+// GET /users
+// func ListUsers(c *gin.Context) {
+// 	var users []entity.User
+// 	if err := entity.DB().Raw("SELECT * FROM users").Scan(&users).Error; err != nil {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		   return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": users})
+// }
+
+// GET /prefixes
+func ListPrefixes(c *gin.Context) {
+	var prefixes []entity.Prefix
+	if err := entity.DB().Raw("SELECT * FROM prefixes").Scan(&prefixes).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": prefixes})
+}
+
+// *******************************************************************************************************
+
+// DELETE /users/:id
+// func DeleteUser(c *gin.Context) {
+// 	id := c.Param("id")
+// 	if tx := entity.DB().Exec("DELETE FROM users WHERE id = ?", id); tx.RowsAffected == 0 {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+// 		   return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": id})
+// }
+
+// DELETE /prefixes/:id
+func DeletePrefix(c *gin.Context) {
+	id := c.Param("id")
+	if tx := entity.DB().Exec("DELETE FROM prefixes WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "prefix not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+
+// *******************************************************************************************************
+
+// PATCH /users
+// func UpdateUser(c *gin.Context) {
+// 	var user entity.User
+// 	if err := c.ShouldBindJSON(&user); err != nil {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		   return
+// 	}
+// 	if tx := entity.DB().Where("id = ?", user.ID).First(&user); tx.RowsAffected == 0 {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+// 		   return
+// 	}
+// 	if err := entity.DB().Save(&user).Error; err != nil {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		   return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": user})
+// }
+
+// PATCH /prefixes
+func UpdatePrefix(c *gin.Context) {
+	var prefix entity.Prefix
+	if err := c.ShouldBindJSON(&prefix); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if tx := entity.DB().Where("id = ?", prefix.ID).First(&prefix); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "prefix not found"})
+		return
+	}
+	if err := entity.DB().Save(&prefix).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": prefix})
+}
