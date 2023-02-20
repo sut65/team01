5d80f54a21924ac29d32251a408f39f674be546a
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-02-20 15:28:26 +0700

    Age validation of HistorySheet - close #89

--- backend/entity/Historysheet_validation_test.go
+++ backend/entity/Historysheet_validation_test.go
@@ -9,33 +9,32 @@ import (
 	"github.com/onsi/gomega"
 )
 
-//	func TestWeightMustMoreThanZeroFloat(t *testing.T) {
-//		g := gomega.NewGomegaWithT(t)
-//		p := "02 Jan 2006"
-//		dob, _ := time.Parse(p, "03 Feb 2056")
-//		fmt.Println(dob)
-//		// loc, _ := time.LoadLocation("Asia/Bangkok")
-//		t.Run("Check Weight must more than 0.00", func(t *testing.T) {
-//			u := HistorySheet{
-//				Weight: -0.05, // false data
-//				Height: 169,
-//				// BMI:                    22.00,
-//				Temperature:            26.00,
-//				SystolicBloodPressure:  60,
-//				DiastolicBloodPressure: 60,
-//				HeartRate:              45,
-//				RespiratoryRate:        32,
-//				OxygenSaturation:       40,
-//				DrugAllergySymtom:      "None Symtom",
-//				PatientSymtom:          "Goodness",
-//				// BirthDay: time.Date(2020,06,14,00,00,00,00,loc),
-//			}
-//			ok, err := govalidator.ValidateStruct(u)
-//			g.Expect(ok).NotTo(gomega.BeTrue())
-//			g.Expect(err).ToNot(gomega.BeNil())
-//			g.Expect(err.Error()).To(gomega.Equal("Weight Invalids"))
-//		})
-//	}
+func TestWeightMustMoreThanZeroFloat(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	p := "02 Jan 2006"
+	dob, _ := time.Parse(p, "03 Feb 2056")
+	fmt.Println(dob)
+	// loc, _ := time.LoadLocation("Asia/Bangkok")
+	t.Run("Check Weight must more than 0.00", func(t *testing.T) {
+		u := HistorySheet{
+			Weight:                 -0.05, // false data
+			Height:                 169,
+			Temperature:            26.00,
+			SystolicBloodPressure:  60,
+			DiastolicBloodPressure: 60,
+			HeartRate:              45,
+			RespiratoryRate:        32,
+			OxygenSaturation:       40,
+			DrugAllergySymtom:      "None Symtom",
+			PatientSymtom:          "Goodness",
+			// BirthDay: time.Date(2020,06,14,00,00,00,00,loc),
+		}
+		ok, err := govalidator.ValidateStruct(u)
+		g.Expect(ok).NotTo(gomega.BeTrue())
+		g.Expect(err).ToNot(gomega.BeNil())
+		g.Expect(err.Error()).To(gomega.Equal("Weight Invalids"))
+	})
+}
 func TestHeartRateMustMoreThanZero(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	p := "02 Jan 2006"
