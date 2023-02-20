2d3552858697689168a41d3fd8e264cd90707ca9
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-02-20 15:10:14 +0700

    edit Interface PatientRegister - close #177

--- frontend/src/models/IPatientRegister/IPatientRegister.ts
+++ frontend/src/models/IPatientRegister/IPatientRegister.ts
@@ -1,83 +1,83 @@
-// import { EmployeesInterface } from "../IEmployee/IEmployee";
+import { EmployeesInterface } from "../IEmployee/IEmployee";
 
-// export interface PatientRegistersInterface {
-//     ID: string,
-//     FirstName: string;
-//     LastName: string;
-//     IdentificationNumber:   string;
-//     Age:    number;
-//     BirthDay: Date | null;
-//     Mobile: string;
-//     Email: string;
-//     Occupation: string;
-//     Address:    string;
+export interface PatientRegistersInterface {
+    ID: string,
+    FirstName: string;
+    LastName: string;
+    IdentificationNumber:   string;
+    Age:    number;
+    BirthDay: Date | null;
+    Mobile: string;
+    Email: string;
+    Occupation: string;
+    Address:    string;
 
-//     EmployeeID: number;
-//     Employee: EmployeesInterface;
+    EmployeeID: number;
+    Employee: EmployeesInterface;
 
-//     GenderID: number;
-//     Gender: GendersInterface;
+    GenderID: number;
+    Gender: GendersInterface;
 
-//     PrefixID: number;
-//     Prefix: PrefixesInterface;
+    PrefixID: number;
+    Prefix: PrefixesInterface;
 
-//     NationalityID: number;
-//     Nationality: NationalitiesInterface;
+    NationalityID: number;
+    Nationality: NationalitiesInterface;
 
-//     ReligionID: number;
-//     Religion: ReligionsInterface;
+    ReligionID: number;
+    Religion: ReligionsInterface;
 
-//     BloodTypeID: number;
-//     BloodType: BloodTypesInterface;
+    BloodTypeID: number;
+    BloodType: BloodTypesInterface;
 
-//     MaritalStatusID: number;
-//     MaritalStatus: MaritalStatusesInterface;
+    MaritalStatusID: number;
+    MaritalStatus: MaritalStatusesInterface;
 
-//     SubDistrictID: number;
-//     SubDistrict: SubDistrictsInterface;
+    SubDistrictID: number;
+    SubDistrict: SubDistrictsInterface;
 
-//     DistrictID: number;
-//     District: DistrictsInterface;
+    DistrictID: number;
+    District: DistrictsInterface;
 
-//     ProvinceID: number;
-//     Province: ProvincesInterface;
-// }
-// export interface GendersInterface {
-//     ID: string;
-//     Name: string;
-// }
-// export interface PrefixesInterface {
-//     ID: string;
-//     Name: string;
-// }
-// export interface NationalitiesInterface {
-//     ID: string;
-//     Name: string;
-// }
-// export interface ReligionsInterface {
-//     ID: string;
-//     Name: string;
-// }
-// export interface BloodTypesInterface {
-//     ID: string;
-//     Name: string;
-// }
-// export interface MaritalStatusesInterface {
-//     ID: string;
-//     Name: string;
-// }
-// export interface SubDistrictsInterface {
-//     ID: string;
-//     Name: string;
-//     PostCode: string;
-//     District: string;
-// }
-// export interface DistrictsInterface {
-//     ID: string;
-//     Name: string;
-//     Province: string;
-// }
-// export interface ProvincesInterface {
-//     ID: string;
-//     Name: string;
-// }
+    ProvinceID: number;
+    Province: ProvincesInterface;
+}
+export interface GendersInterface {
+    ID: string;
+    Name: string;
+}
+export interface PrefixesInterface {
+    ID: string;
+    Name: string;
+}
+export interface NationalitiesInterface {
+    ID: string;
+    Name: string;
+}
+export interface ReligionsInterface {
+    ID: string;
+    Name: string;
+}
+export interface BloodTypesInterface {
+    ID: string;
+    Name: string;
+}
+export interface MaritalStatusesInterface {
+    ID: string;
+    Name: string;
+}
+export interface SubDistrictsInterface {
+    ID: string;
+    Name: string;
+    PostCode: string;
+    District: string;
+}
+export interface DistrictsInterface {
+    ID: string;
+    Name: string;
+    Province: string;
+}
+export interface ProvincesInterface {
+    ID: string;
+    Name: string;
+}
