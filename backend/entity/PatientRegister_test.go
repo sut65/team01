65f9c928acc77a2e3249322612745cbf483d8075
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-02-20 14:50:25 +0700

    built Birth Day validation of PatientRegister - close #65

--- backend/entity/PatientRegister_test.go
+++ backend/entity/PatientRegister_test.go
@@ -9,29 +9,29 @@ import (
 	"github.com/onsi/gomega"
 )
 
-//	func TestBirthDayMustNotBeFuture(t *testing.T) {
-//		g := gomega.NewGomegaWithT(t)
-//		// dob := time.Now().Add(time.Hour * -24)
-//		p := "02 Jan 2006"
-//		dob, _ := time.Parse(p, "03 Feb 2056")
-//		t.Run("Check Date of Birth must not be the future", func(t *testing.T) {
-//			u := PatientRegister{
-//				FirstName:            "Teerasil",
-//				LastName:             "Daengda",
-//				IdentificationNumber: "1234567898123",
-//				Age:                  23,
-//				BirthDay:             dob,
-//				Mobile:               "0621539985",
-//				Occupation:           "Football Player",
-//				Address:              "BG Pathum United",
-//				// BirthDay: time.Date(2020,06,14,00,00,00,00,loc),
-//			}
-//			ok, err := govalidator.ValidateStruct(u)
-//			g.Expect(ok).NotTo(gomega.BeTrue())
-//			g.Expect(err).ToNot(gomega.BeNil())
-//			g.Expect(err.Error()).To(gomega.Equal("Birth Day Invalids"))
-//		})
-//	}
+func TestBirthDayMustNotBeFuture(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	// dob := time.Now().Add(time.Hour * -24)
+	p := "02 Jan 2006"
+	dob, _ := time.Parse(p, "03 Feb 2056")
+	t.Run("Check Date of Birth must not be the future", func(t *testing.T) {
+		u := PatientRegister{
+			FirstName:            "Teerasil",
+			LastName:             "Daengda",
+			IdentificationNumber: "1234567898123",
+			Age:                  23,
+			BirthDay:             dob,
+			Mobile:               "0621539985",
+			Occupation:           "Football Player",
+			Address:              "BG Pathum United",
+			// BirthDay: time.Date(2020,06,14,00,00,00,00,loc),
+		}
+		ok, err := govalidator.ValidateStruct(u)
+		g.Expect(ok).NotTo(gomega.BeTrue())
+		g.Expect(err).ToNot(gomega.BeNil())
+		g.Expect(err.Error()).To(gomega.Equal("Birth Day Invalids"))
+	})
+}
 func TestAgeMustBeNotNegativeInteger(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	p := "02 Jan 2006"
