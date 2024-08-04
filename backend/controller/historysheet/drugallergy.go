f83717f4d13a2c0bed4f77057807874f613d8c34
watcharaphol1938 <jeanswc1938@gmail.com>
2024-08-04 14:12:41 +0700

    modified historysheet controller - close #222

--- backend/controller/historysheet/drugallergy.go
+++ backend/controller/historysheet/drugallergy.go
@@ -7,21 +7,6 @@ import (
 	"github.com/sut65/team01/entity"
 )
 
-// /*	POST /users
-// 	func CreateUser(c *gin.Context) {
-// 		var user entity.User
-// 		if err := c.ShouldBindJSON(&user); err != nil {
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 			return
-// 		}
-// 		if err := entity.DB().Create(&user).Error; err != nil {
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 			return
-// 		}
-// 		c.JSON(http.StatusOK, gin.H{"data": user})
-// 	}
-// */
-
 // POST /drugallergies
 func CreateDrugAllergy(c *gin.Context) {
 	var drugallergy entity.DrugAllergy
@@ -36,19 +21,6 @@ func CreateDrugAllergy(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": drugallergy})
 }
 
-// *******************************************************************************************************
-
-// GET /user/:id
-// func GetUser(c *gin.Context) {
-// 	var user entity.User
-// 	id := c.Param("id")
-// 	if err := entity.DB().Raw("SELECT * FROM users WHERE id = ?", id).Scan(&user).Error; err != nil {
-// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		   return
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{"data": user})
-// }
-
 // GET /drugallergy/:id
 func GetDrugAllergy(c *gin.Context) {
 	var drugallergy entity.DrugAllergy
@@ -60,18 +32,6 @@ func GetDrugAllergy(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": drugallergy})
 }
 
-// *******************************************************************************************************
-
-// GET /users
-// func ListUsers(c *gin.Context) {
-// 	var users []entity.User
-// 	if err := entity.DB().Raw("SELECT * FROM users").Scan(&users).Error; err != nil {
-// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		   return
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{"data": users})
-// }
-
 // GET /drugallergies
 func ListDrugAllergies(c *gin.Context) {
 	var drugallergies []entity.DrugAllergy
@@ -82,18 +42,6 @@ func ListDrugAllergies(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": drugallergies})
 }
 
-// *******************************************************************************************************
-
-// DELETE /users/:id
-// func DeleteUser(c *gin.Context) {
-// 	id := c.Param("id")
-// 	if tx := entity.DB().Exec("DELETE FROM users WHERE id = ?", id); tx.RowsAffected == 0 {
-// 		   c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
-// 		   return
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{"data": id})
-// }
-
 // DELETE /drugallergies/:id
 func DeleteDrugAllergy(c *gin.Context) {
 	id := c.Param("id")
@@ -103,26 +51,6 @@ func DeleteDrugAllergy(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{"data": id})
 }
-
-// *******************************************************************************************************
-
-// PATCH /users
-// func UpdateUser(c *gin.Context) {
-// 	var user entity.User
-// 	if err := c.ShouldBindJSON(&user); err != nil {
-// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		   return
-// 	}
-// 	if tx := entity.DB().Where("id = ?", user.ID).First(&user); tx.RowsAffected == 0 {
-// 		   c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
-// 		   return
-// 	}
-// 	if err := entity.DB().Save(&user).Error; err != nil {
-// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		   return
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{"data": user})
-// }
 
 // PATCH /drugallergies
 func UpdateDrugAllergy(c *gin.Context) {
